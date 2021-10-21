4a60e235a93e1e4358ab5b1a64dbdc5615a0fbb5
juancho5356 <88491443+juancho5356@users.noreply.github.com>
2021-10-21 11:36:07 -0300

    Add files via upload

---
+++ databaseManager/DAO_Formulario.java
@@ -0,0 +1,79 @@
+package Controlador;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.LinkedList;
+
+import Modelo.Administrador;
+import Modelo.Aficionado;
+import Modelo.Formulario;
+import Modelo.Investigador;
+
+public class DAO_Formulario {
+
+	private static final String ALL_FORMULARIOS = "SELECT * FROM FORMULARIOS ORDER BY 1";
+	private static final String DELETE_FORMULARIO = "DELETE FROM FORMULARIOS WHERE ID_FORMULARIO = ?";
+	
+	public static boolean delete(int id) {
+		try {
+			
+			PreparedStatement statement = DatabaseManager.getConnection().prepareStatement(DELETE_FORMULARIO);
+			
+			statement.setInt(1, id);
+			
+			int Retorno = statement.executeUpdate();
+			
+			return Retorno > 0;
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static LinkedList<Formulario> findAll(){
+		LinkedList<Formulario> formularios = new LinkedList<>();
+		
+		try {
+			PreparedStatement statement = DatabaseManager.getConnection().prepareStatement(ALL_FORMULARIOS);
+			ResultSet resultado = statement.executeQuery();
+			
+			while (resultado.next()) {
+				Formulario formulario = getFormularioFromResultSet(resultado);
+				formularios.add(formulario);
+			}
+			return formularios;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private static Formulario getFormularioFromResultSet(ResultSet resultado) throws SQLException {
+		
+		int id = resultado.getInt("ID_FORMULARIO");
+		Date fecha = resultado.getDate("FECHA");
+		String ubicacion = resultado.getString("UBICACION");
+		String nombreVariable = resultado.getString("NOMBRE_VARIABLE");
+		String metodoMedicion = resultado.getString("METODO_MEDICION");
+		int idAdm = resultado.getInt("ID_ADMINISTRADOR");
+		int idInv = resultado.getInt("ID_INVESTIGADOR");
+		int idAfi = resultado.getInt("ID_AFICIONADO");
+		
+		Administrador adm = new Administrador();
+		adm.setIdAdministrador(idAdm);
+		
+		Investigador inv = new Investigador();
+		inv.setIdInvestigador(idInv);
+		
+		Aficionado afi = new Aficionado();
+		afi.setIdAficionado(idAfi);
+		
+		Formulario f = new Formulario(id, fecha, ubicacion, nombreVariable, metodoMedicion, adm, inv, afi);
+		
+		return f;
+	}
+}
